a753f46307f38599fb3640a9bf01d433b8301fdf
MexxMike <MexxMike@c9373d24-334e-0410-853f-19ce1f673a9b>
2008-05-27 12:14:09 +0000

    Updated units base classes.

--- trunk/WM/Units/HumanOid.cs
+++ trunk/WM/Units/HumanOid.cs
@@ -5,7 +5,8 @@ namespace WM.Units
 {
     class HumanOid : UnitBase
     {
-        public HumanOid()
+        public HumanOid(Vector2 position, float rotation, Vector2 scale, string textureAsset)
+            : base(position, rotation, scale, textureAsset)
         {            
         }
 
