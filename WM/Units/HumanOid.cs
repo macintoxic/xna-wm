72c04b5636af0f0da72e66a8b1855a8602d74e71
MexxMike <MexxMike@c9373d24-334e-0410-853f-19ce1f673a9b>
2008-05-27 11:53:28 +0000

    added unit base class

---
+++ WM/Units/HumanOid.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WM.Units
+{
+    class HumanOid : Microsoft.Xna.Framework.Game
+    {
+        public HumanOid()
+        {
+            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            
+        }
+
+    }
+}
