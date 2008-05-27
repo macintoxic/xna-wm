1927fd24682ca8c8d79ca7cd71db89aeb3ca6085
walter.tamboer <walter.tamboer@c9373d24-334e-0410-853f-19ce1f673a9b>
2008-05-27 09:49:44 +0000

    The first commit... merged the Tiled example and the Game State example.

---
+++ trunk/WM/WMGame.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using WM.Screens;
+
+namespace WM
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class WMGame : Microsoft.Xna.Framework.Game
+    {
+        /// <summary>
+        /// The graphics device manager used to draw everything.
+        /// </summary>
+        private GraphicsDeviceManager graphics;
+
+        /// <summary>
+        /// The screen manager is used to manage, right, screens.
+        /// </summary>
+        private ScreenManager screenManager;
+
+        /// <summary>
+        /// Initializes a new instance of this class.
+        /// </summary>
+        public WMGame()
+        {
+            Content.RootDirectory = "Content";
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+
+            // Create the screen manager component.
+            screenManager = new ScreenManager(this);
+            Components.Add(screenManager);
+
+            // Activate the first screens.
+            screenManager.AddScreen(new BackgroundScreen());
+            screenManager.AddScreen(new MainMenuScreen());
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.Black);
+
+            // The real drawing happens inside the screen manager component.
+            base.Draw(gameTime);
+        }
+    }
+}
